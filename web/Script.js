b3338909c693362e1e46af349d410914a7047ffc
M3ko <164372984+le-tonk@users.noreply.github.com>
2024-10-10 15:18:49 -0300

    Add files via upload

--- web/Script.js
+++ web/Script.js
@@ -19,21 +19,41 @@ function show1(){
     document.getElementById('ch2').style.backgroundColor ='white';
     document.getElementById('ch3').style.backgroundColor ='white';
     document.getElementById('ch4').style.backgroundColor ='white';
+    document.getElementById('ch5').style.backgroundColor ='white';
+    document.getElementById('ch6').style.backgroundColor ='white';
+    document.getElementById('ch7').style.backgroundColor ='white';
+    document.getElementById('ch8').style.backgroundColor ='white';
+    document.getElementById('ch9').style.backgroundColor ='white';
     document.getElementById('prod1').style.display ='block';
     document.getElementById('prod2').style.display ='none';
     document.getElementById('prod3').style.display ='none';
     document.getElementById('prod4').style.display ='none';
+    document.getElementById('prod5').style.display ='none';
+    document.getElementById('prod6').style.display ='none';
+    document.getElementById('prod7').style.display ='none';
+    document.getElementById('prod8').style.display ='none';
+    document.getElementById('prod9').style.display ='none';
     console.log("showing dragons")
   }
 function show2(){
-    document.getElementById('ch1').style.backgroundColor ='white';
-    document.getElementById('ch2').style.backgroundColor ='blue';
-    document.getElementById('ch3').style.backgroundColor ='white';
-    document.getElementById('ch4').style.backgroundColor ='white';
-    document.getElementById('prod1').style.display ='none';
-    document.getElementById('prod2').style.display ='block';
-    document.getElementById('prod3').style.display ='none';
-    document.getElementById('prod4').style.display ='none';
+  document.getElementById('ch1').style.backgroundColor ='white';
+  document.getElementById('ch2').style.backgroundColor ='blue';
+  document.getElementById('ch3').style.backgroundColor ='white';
+  document.getElementById('ch4').style.backgroundColor ='white';
+  document.getElementById('ch5').style.backgroundColor ='white';
+  document.getElementById('ch6').style.backgroundColor ='white';
+  document.getElementById('ch7').style.backgroundColor ='white';
+  document.getElementById('ch8').style.backgroundColor ='white';
+  document.getElementById('ch9').style.backgroundColor ='white';
+  document.getElementById('prod1').style.display ='none';
+  document.getElementById('prod2').style.display ='block';
+  document.getElementById('prod3').style.display ='none';
+  document.getElementById('prod4').style.display ='none';
+  document.getElementById('prod5').style.display ='none';
+  document.getElementById('prod6').style.display ='none';
+  document.getElementById('prod7').style.display ='none';
+  document.getElementById('prod8').style.display ='none';
+  document.getElementById('prod9').style.display ='none';
     console.log("showing fire")
   }
 
@@ -42,18 +62,149 @@ function show2(){
     document.getElementById('ch2').style.backgroundColor ='white';
     document.getElementById('ch3').style.backgroundColor ='blue';
     document.getElementById('ch4').style.backgroundColor ='white';
+    document.getElementById('ch5').style.backgroundColor ='white';
+    document.getElementById('ch6').style.backgroundColor ='white';
+    document.getElementById('ch7').style.backgroundColor ='white';
+    document.getElementById('ch8').style.backgroundColor ='white';
+    document.getElementById('ch9').style.backgroundColor ='white';
     document.getElementById('prod1').style.display ='none';
     document.getElementById('prod2').style.display ='none';
     document.getElementById('prod3').style.display ='block';
     document.getElementById('prod4').style.display ='none';
+    document.getElementById('prod5').style.display ='none';
+    document.getElementById('prod6').style.display ='none';
+    document.getElementById('prod7').style.display ='none';
+    document.getElementById('prod8').style.display ='none';
+    document.getElementById('prod9').style.display ='none';
+    console.log("showing grass")
   }
 function show4(){
+  document.getElementById('ch1').style.backgroundColor ='white';
+    document.getElementById('ch2').style.backgroundColor ='white';
+        document.getElementById('ch3').style.backgroundColor ='white';
+              document.getElementById('ch4').style.backgroundColor ='blue';
+                        document.getElementById('ch5').style.backgroundColor ='white';
+                                        document.getElementById('ch6').style.backgroundColor ='white';
+                                                                  document.getElementById('ch7').style.backgroundColor ='white';
+                                                                                                            document.getElementById('ch8').style.backgroundColor ='white';
+                                                                                                                                                                                document.getElementById('ch9').style.backgroundColor ='white';
+                                                                                                                                                                                document.getElementById('prod1').style.display ='none';
+                                                                                                            document.getElementById('prod2').style.display ='none';
+                                                                  document.getElementById('prod3').style.display ='none';
+                                        document.getElementById('prod4').style.display ='block';
+                        document.getElementById('prod5').style.display ='none';
+              document.getElementById('prod6').style.display ='none';
+        document.getElementById('prod7').style.display ='none';
+    document.getElementById('prod8').style.display ='none';
+  document.getElementById('prod9').style.display ='none';
+  console.log("showing neutral")
+  }
+  function show5(){
     document.getElementById('ch1').style.backgroundColor ='white';
     document.getElementById('ch2').style.backgroundColor ='white';
     document.getElementById('ch3').style.backgroundColor ='white';
-    document.getElementById('ch4').style.backgroundColor ='blue';
+    document.getElementById('ch4').style.backgroundColor ='white';
+    document.getElementById('ch5').style.backgroundColor ='blue';
+    document.getElementById('ch6').style.backgroundColor ='white';
+    document.getElementById('ch7').style.backgroundColor ='white';
+    document.getElementById('ch8').style.backgroundColor ='white';
+    document.getElementById('ch9').style.backgroundColor ='white';
     document.getElementById('prod1').style.display ='none';
     document.getElementById('prod2').style.display ='none';
     document.getElementById('prod3').style.display ='none';
-    document.getElementById('prod4').style.display ='block';
+    document.getElementById('prod4').style.display ='none';
+    document.getElementById('prod5').style.display ='block';
+    document.getElementById('prod6').style.display ='none';
+    document.getElementById('prod7').style.display ='none';
+    document.getElementById('prod8').style.display ='none';
+    document.getElementById('prod9').style.display ='none';
+    console.log("showing eletric")
+  }
+
+  function show6(){
+    document.getElementById('ch1').style.backgroundColor ='white';
+    document.getElementById('ch2').style.backgroundColor ='white';
+    document.getElementById('ch3').style.backgroundColor ='white';
+    document.getElementById('ch4').style.backgroundColor ='white';
+    document.getElementById('ch5').style.backgroundColor ='white';
+    document.getElementById('ch6').style.backgroundColor ='blue';
+    document.getElementById('ch7').style.backgroundColor ='white';
+    document.getElementById('ch8').style.backgroundColor ='white';
+    document.getElementById('ch9').style.backgroundColor ='white';
+    document.getElementById('prod1').style.display ='none';
+    document.getElementById('prod2').style.display ='none';
+    document.getElementById('prod3').style.display ='none';
+    document.getElementById('prod4').style.display ='none';
+    document.getElementById('prod5').style.display ='none';
+    document.getElementById('prod6').style.display ='block';
+    document.getElementById('prod7').style.display ='none';
+    document.getElementById('prod8').style.display ='none';
+    document.getElementById('prod9').style.display ='none';
+    console.log("showing ground")
+  }
+
+  function show7(){
+    document.getElementById('ch1').style.backgroundColor ='white';
+    document.getElementById('ch2').style.backgroundColor ='white';
+    document.getElementById('ch3').style.backgroundColor ='white';
+    document.getElementById('ch4').style.backgroundColor ='white';
+    document.getElementById('ch5').style.backgroundColor ='white';
+    document.getElementById('ch6').style.backgroundColor ='white';
+    document.getElementById('ch7').style.backgroundColor ='blue';
+    document.getElementById('ch8').style.backgroundColor ='white';
+    document.getElementById('ch9').style.backgroundColor ='white';
+    document.getElementById('prod1').style.display ='none';
+    document.getElementById('prod2').style.display ='none';
+    document.getElementById('prod3').style.display ='none';
+    document.getElementById('prod4').style.display ='none';
+    document.getElementById('prod5').style.display ='none';
+    document.getElementById('prod6').style.display ='none';
+    document.getElementById('prod7').style.display ='block';
+    document.getElementById('prod8').style.display ='none';
+    document.getElementById('prod9').style.display ='none';
+    console.log("showing ice")
+  }
+
+  function show8(){
+    document.getElementById('ch1').style.backgroundColor ='white';
+    document.getElementById('ch2').style.backgroundColor ='white';
+    document.getElementById('ch3').style.backgroundColor ='white';
+    document.getElementById('ch4').style.backgroundColor ='white';
+    document.getElementById('ch5').style.backgroundColor ='white';
+    document.getElementById('ch6').style.backgroundColor ='white';
+    document.getElementById('ch7').style.backgroundColor ='white';
+    document.getElementById('ch8').style.backgroundColor ='blue';
+    document.getElementById('ch9').style.backgroundColor ='white';
+    document.getElementById('prod1').style.display ='none';
+    document.getElementById('prod2').style.display ='none';
+    document.getElementById('prod3').style.display ='none';
+    document.getElementById('prod4').style.display ='none';
+    document.getElementById('prod5').style.display ='none';
+    document.getElementById('prod6').style.display ='none';
+    document.getElementById('prod7').style.display ='none';
+    document.getElementById('prod8').style.display ='block';
+    document.getElementById('prod9').style.display ='none';
+    console.log("showing dark")
+  }
+  
+  function show9(){
+    document.getElementById('ch1').style.backgroundColor ='white';
+    document.getElementById('ch2').style.backgroundColor ='white';
+    document.getElementById('ch3').style.backgroundColor ='white';
+    document.getElementById('ch4').style.backgroundColor ='white';
+    document.getElementById('ch5').style.backgroundColor ='white';
+    document.getElementById('ch6').style.backgroundColor ='white';
+    document.getElementById('ch7').style.backgroundColor ='white';
+    document.getElementById('ch8').style.backgroundColor ='white';
+    document.getElementById('ch9').style.backgroundColor ='blue';
+    document.getElementById('prod1').style.display ='none';
+    document.getElementById('prod2').style.display ='none';
+    document.getElementById('prod3').style.display ='none';
+    document.getElementById('prod4').style.display ='none';
+    document.getElementById('prod5').style.display ='none';
+    document.getElementById('prod6').style.display ='none';
+    document.getElementById('prod7').style.display ='none';
+    document.getElementById('prod8').style.display ='none';
+    document.getElementById('prod9').style.display ='block';
+    console.log("showing water")
   }

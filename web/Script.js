ada9e21b8220afe279c755918c3b3af9ca7f7823
ThePedroo <pedroolimpioguerra@gmail.com>
2024-10-20 13:22:46 -0300

    add: pegar produtos do servidor
    Este commit adiciona a funcionalidade cujo faz os produtos serem pegos
    do servidor.

--- web/Script.js
+++ web/Script.js
@@ -1,23 +1,3 @@
-let slideIndex = 0
-showSlides();
-
-// INFO: Currently not usable
-function showSlides() {
-  let i
-  let slides = document.getElementsByClassName("slide")
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"
-  }
-
-  slideIndex++
-  if (slideIndex > slides.length) { slideIndex = 1 }
-
-  slides[slideIndex - 1].style.display = "block"
-
-  setTimeout(showSlides, 2000)
-}
-
 function showItem(number) {
   const chs = document.getElementsByClassName('chs')
   const prods = document.getElementsByClassName('prods')
@@ -30,3 +10,96 @@ function showItem(number) {
   chs[number - 1].style.backgroundColor = 'blue'
   prods[number - 1].style.display = 'block'
 }
+
+function fetchProdutos() {
+  fetch('http://localhost:8888/api/produtos/lista').then((response) => response.json()).then((data) => {
+    /* INFO: Cria uma lista de produtos */
+    const ul = document.createElement('ul');
+    document.body.appendChild(ul);
+
+    const tipos = {}
+    data.forEach((produto) => {
+      /* INFO: Cria um item de lista para cada produto */
+      if (!tipos[produto.tipo]) tipos[produto.tipo] = []
+
+      tipos[produto.tipo].push(produto)
+    })
+
+    const filter = document.getElementById('filter')
+    Object.keys(tipos).forEach((tipo, index) => {
+      /*
+        <label for="ch1" class="tipo_palft">
+          <input type="radio" name="tab" id="ch1" class="chs" onclick="showItem(1)">
+          Dragon
+        </label>
+      */
+      const ch = document.createElement('label')
+      ch.htmlFor = `ch${index}`
+      ch.classList.add('tipo_palft')
+
+      filter.appendChild(ch)
+
+      const input = document.createElement('input')
+      input.type = 'radio'
+      input.name = 'tab'
+      input.id = `ch${index}`
+      input.classList.add('chs')
+      input.onclick = () => showItem(index + 1)
+
+      ch.appendChild(input)
+
+      const tipoCapitalized = tipo.charAt(0).toUpperCase() + tipo.slice(1)
+
+      const text = document.createTextNode(tipoCapitalized)
+      ch.appendChild(text)
+    })
+
+    const product_area = document.getElementById('product_area')
+    Object.keys(tipos).forEach((tipo) => {
+      /*                 
+        <div class="prods">
+          <div class="content">
+            <img src="images/fire/71.png" alt="" id="palft" class="fire">
+            <button onclick="" class="pal_button">Comprar</button>
+          </div>
+          <div class="content">
+            <img src="images/fire/58.png" alt="" id="palft" class="fire">
+            <button onclick="" class="pal_button">Comprar</button>
+          </div>
+        </div> 
+      */
+      const prods = document.createElement('div')
+      prods.classList.add('prods')
+
+      product_area.appendChild(prods)
+
+      tipos[tipo].forEach((produto) => {
+        const content = document.createElement('div')
+        content.classList.add('content')
+
+        prods.appendChild(content)
+
+        const img = document.createElement('img')
+        img.src = `images/${produto.tipo}/${produto.id}.png`
+        img.alt = ''
+        img.id = 'palft'
+        img.classList.add(produto.tipo)
+
+        content.appendChild(img)
+
+        const button = document.createElement('button')
+        button.onclick = () => {
+          /* TODO: Adicionar produto ao carrinho */
+        }
+        button.classList.add('pal_button')
+
+        const text = document.createTextNode('Comprar')
+        button.appendChild(text)
+
+        content.appendChild(button)
+      })
+    })
+  })
+}
+
+fetchProdutos()
